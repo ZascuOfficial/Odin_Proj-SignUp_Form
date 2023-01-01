deb0ddc50a7e9780b6660b21606646dadfe60efe
Zascu <zascu.github@gmail.com>
2023-01-01 21:45:18 +0200

    Fix incorrect file path

--- signup-form/src/js/App.jsx
+++ signup-form/src/js/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Banner from './components/Banner';
-import MainSection from './MainSection';
+import MainSection from './components/MainSection';
 
 export default function App() {
   return (
