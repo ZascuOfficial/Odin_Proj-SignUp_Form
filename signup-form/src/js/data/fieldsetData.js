13eab0478f6385109713b8a1713b60a0ceaf6f24
Zascu <zascu.github@gmail.com>
2023-01-01 22:05:49 +0200

    Fill in the initial data for the fieldsets

---
+++ signup-form/src/js/data/fieldsetData.js
@@ -0,0 +1,39 @@
+const fieldsetData = [
+  {
+    name: 'first-name',
+    label: 'First Name',
+    type: 'text'
+  },
+
+  {
+    name: 'last-name',
+    label: 'Last Name',
+    type: 'text'
+  },
+
+  {
+    name: 'email',
+    label: 'Email',
+    type: 'email'
+  },
+
+  {
+    name: 'phone-number',
+    label: 'Phone Number',
+    type: 'tel'
+  },
+
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password'
+  },
+
+  {
+    name: 'confirm-password',
+    label: 'Confirm Password',
+    type: 'password'
+  }
+];
+
+export default fieldsetData;
