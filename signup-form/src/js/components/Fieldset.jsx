21c38e2554aa4a12704bec454f0efe2d36b31761
Zascu <zascu.github@gmail.com>
2023-01-01 21:54:38 +0200

    Create the reusable fieldset component

---
+++ signup-form/src/js/components/Fieldset.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Fieldset(props) {
+  return (
+    <fieldset>
+      <label htmlFor={props.name}>{props.label}</label>
+
+      <input
+        type={props.type}
+        name={props.name}
+        id={props.name}
+      />
+    </fieldset>
+  );
+}
