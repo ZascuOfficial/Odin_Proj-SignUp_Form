41039c5bd3b8eea08b9da1ba9812e94c10ae8861
Zascu <zascu.github@gmail.com>
2023-01-01 19:20:47 +0200

    Prepare to render the banner and the main section

---
+++ signup-form/src/js/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Banner from './components/Banner';
+import MainSection from './MainSection';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Banner />
+      <MainSection />
+    </div>
+  );
+}
