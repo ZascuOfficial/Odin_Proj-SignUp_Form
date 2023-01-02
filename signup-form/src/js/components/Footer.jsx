9c68be07003ef3edee7b409539412defe680f6bd
Zascu <zascu.github@gmail.com>
2023-01-02 20:33:51 +0200

    Create a footer element

---
+++ signup-form/src/js/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <p className="login">
+      Already own an account?
+      <a href="#">Log in</a>
+    </p>
+  );
+}
